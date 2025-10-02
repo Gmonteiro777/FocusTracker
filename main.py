0a34bbcb156c963885f69963beb826ca66029715
Gmonteiro777 <guizinhomont2k20@icloud.com>
2025-10-02 17:41:55 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from focus_logger import monitor_focus
+
+if __name__ == "__main__":
+    print("🔎 Monitorando foco... (CTRL + C para parar)")
+    monitor_focus(main_app="Visual Studio Code")  # aqui você pode trocar o app principal
